bcce6eba21b5c63662d8acef295513cd0e5b735d
AndreLobo1 <andre.paula@sou.inteli.edu.br>
2025-05-27 10:00:11 -0300

    [EP]

--- 2025-1A-T09-ES09-G05-main/src/test/features/steps/data_alocacao.py
+++
@@ -1,3 +0,0 @@
-MAX_TEMPO_BUSCA = 15  # Tempo máximo permitido para busca em minutos
-RAIO_INICIAL = 500  # Raio inicial de busca em metros
-INCREMENTO_RAIO = 500  # A cada minuto, o raio aumenta 500 metros
